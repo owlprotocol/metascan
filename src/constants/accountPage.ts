a3e847c74e602d205d2547c25df95c0bc10695fd
HrikB <hrik.bhowal@gmail.com>
2021-12-24 13:19:06 -0800

    HashRouting for Account page

---
+++ src/constants/accountPage.ts
@@ -0,0 +1,7 @@
+export const ACCOUNT_DETAILS = [
+    { href: '', label: 'Transactions' },
+    { href: 'internaltx', label: 'Internal Txns' },
+    { href: 'tokentxns', label: 'Erc20 Token Txns' },
+    { href: 'tokentxnsErc721', label: 'Erc721 Token Txns' },
+    { href: 'comments', label: 'Comments' },
+];
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,4 +1,5 @@
 import THEME_COLORS from './themeColors';
 import { NAV_LINKS, STUB_TOKEN_LIST } from './landingPage';
+import { ACCOUNT_DETAILS } from './accountPage';
 
-export { THEME_COLORS, NAV_LINKS, STUB_TOKEN_LIST };
+export { THEME_COLORS, NAV_LINKS, STUB_TOKEN_LIST, ACCOUNT_DETAILS };
