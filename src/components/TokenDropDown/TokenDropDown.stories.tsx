280ce8d0105ef9485267dcb30d47c6c85fca7f8b
Leo Vigna <leo.vigna@gmail.com>
2022-02-03 10:09:40 +0100

    TokenDropDown

--- src/components/TokenDropDown/TokenDropDown.stories.tsx
+++ src/components/TokenDropDown/TokenDropDown.stories.tsx
@@ -1,16 +1,24 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { withThemeProvider } from '../../hoc';
-import TokenDropDown, { Props } from '.';
+import { ADDRESS_0 } from '@owlprotocol/web3-redux/test/data';
+import Web3 from 'web3';
+import { Network } from '@owlprotocol/web3-redux';
+import { MAINNET_RPC } from '@owlprotocol/web3-redux/environment';
+import { withThemeProvider, withStoreProvider, withMockData } from '../../hoc';
+import { TokenDropDown, Props } from '.';
 
-const Wrapper = withThemeProvider((props: Props) => {
-    return <TokenDropDown {...props} />;
-});
+const network: Network.Network = { networkId: '1', web3: new Web3(MAINNET_RPC) };
+const actions = [Network.create(network)];
+
+const Wrapper = withThemeProvider(withStoreProvider(withMockData(TokenDropDown, actions)));
+const Template: ComponentStory<typeof TokenDropDown> = (args: any) => <Wrapper {...args} />;
+export const Main = Template.bind({});
+const Args: Props = {
+    networkId: '1',
+    accountAddress: ADDRESS_0,
+};
+Main.args = Args;
 
 export default {
-    title: 'Generic/TokenDropDown',
+    title: 'Token/TokenDropDown',
     component: TokenDropDown,
 } as ComponentMeta<typeof TokenDropDown>;
-
-const Template: ComponentStory<typeof TokenDropDown> = (args: any) => <Wrapper {...args} />;
-
-export const Main = Template.bind({});
--- src/components/TokenDropDown/index.tsx
+++ src/components/TokenDropDown/index.tsx
@@ -1,30 +1,48 @@
-import web3 from 'web3';
-import { useNetworkCreate, useFetchAccountTokens } from '../../hooks';
+import { fromWei } from 'web3-utils';
+import { useERC20 } from '@owlprotocol/web3-redux/contract/hooks';
+
+export interface TokenDropDownOptionProps {
+    networkId: string;
+    tokenAddress: string;
+    accountAddress: string;
+}
+export const TokenDropDownOption = ({ networkId, tokenAddress, accountAddress }: TokenDropDownOptionProps) => {
+    const { symbol, balanceOf } = useERC20(networkId, tokenAddress, accountAddress);
+    const balanceOfEth = balanceOf ? fromWei(balanceOf) : '0.0';
+    return (
+        <option key={tokenAddress}>
+            {balanceOfEth} {symbol}
+        </option>
+    );
+};
 
 export interface Props {
-    tokenList?: [];
-    accountAddr: string;
     networkId: string;
+    accountAddress: string;
+    tokenAddressList?: string[];
 }
 
-function TokenDropDown({ accountAddr, networkId }: Props) {
-    useNetworkCreate();
-    const tokenList = useFetchAccountTokens(networkId, accountAddr);
+const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
+const defaultTokenList = [WETH];
 
+export const TokenDropDown = ({ networkId, accountAddress, tokenAddressList = defaultTokenList }: Props) => {
     return (
         <div className="flex">
             <div>Tokens</div>
             <div style={{ width: '72%' }}>
                 <select>
-                    {tokenList.map((token) => (
-                        <option key={token.address}>
-                            {token.currAccBal ? web3.utils.fromWei(token.currAccBal) : '0'} {token.symbol}
-                        </option>
+                    {tokenAddressList.map((tokenAddress) => (
+                        <TokenDropDownOption
+                            key={tokenAddress}
+                            networkId={networkId}
+                            accountAddress={accountAddress}
+                            tokenAddress={tokenAddress}
+                        />
                     ))}
                 </select>
             </div>
         </div>
     );
-}
+};
 
 export default TokenDropDown;
