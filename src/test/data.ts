606e69d24f0f420cb4d2c5ec4fd798e44b6e8867
Leo Vigna <leo.vigna@gmail.com>
2022-02-07 00:45:02 +0100

    remove MAINNET_RPC enforcement

--- src/test/data.ts
+++ src/test/data.ts
@@ -1,13 +1,11 @@
-import Web3 from 'web3';
 import { toWei } from 'web3-utils';
 import { Network, Contract } from '@owlprotocol/web3-redux';
-import { MAINNET_RPC } from '@owlprotocol/web3-redux/environment';
 
 export const ADDRESS_0 = '0x0000000000000000000000000000000000000000';
 export const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 export const VULCAN_OCR = '0xD22c87Dc7a3F12dcBB75CEbDA2e96f6766AE114F';
 
-export const network1 = { networkId: '1', web3: new Web3(MAINNET_RPC!) };
+export const network1 = { networkId: '1' };
 export const contract1 = {
     networkId: '1',
     address: ADDRESS_0,
